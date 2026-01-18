e36bf4370f6c018c74af83a7ff96c272259dad0f
nazephyrus <126683488+naden01@users.noreply.github.com>
2026-01-18 11:17:18 +0700

    import usb init from nikel
    https://github.com/PitchBlackRecoveryProject/android_device_xiaomi_nikel-pbrp/blob/android-6.0/recovery/root/init.recovery.usb.rc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,7 @@ TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXCLUDE_TWRPAPP := false
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_DEVICE_VERSION :=  nazephyrus | cheap ahh tablet

0adb124ea359494caaeaddde2b8d2e51cb7478c4
nazephyrus <126683488+naden01@users.noreply.github.com>
2026-01-18 12:51:33 +0700

    set custom cpu and battery path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,8 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TW_CUSTOM_BATTERY_PATH := /sys/devices/platform/battery/power_supply/battery/capacity
 TW_DEVICE_VERSION :=  nazephyrus | cheap ahh tablet
 
 # exFAT FS Support
